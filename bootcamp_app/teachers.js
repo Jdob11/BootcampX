c5ea843ef675e2dcaebc7021730c6bf1f54e3413
Jdob11 <jbdobson11@gmail.com>
2024-04-10 11:01:26 -0700

    feat: Install node-postgres library and create JavaScript queries for students.js and teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,25 +9,26 @@ const pool = new Pool({
 });
 
 const cohortName = args[0];
-const limit = args[1];
 
 pool
   .query(
     `
-SELECT students.id AS student_id,
-       students.name AS student_name,
-       cohorts.name AS cohort_name
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE $1
-LIMIT $2;
+SELECT DISTINCT teachers.name AS teacher,
+                cohorts.name AS cohort
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+JOIN assistance_requests ON students.id = student_id
+JOIN teachers ON teachers.id = teacher_id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+
 `,
-  [cohortName + '%', limit || 5]
+  [cohortName || 'JUL02']
 )
 .then((res) => {
   res.rows.forEach((user) => {
     console.log(
-      `${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`
+      `${user.cohort}: ${user.teacher}`
     );
   });
 })
